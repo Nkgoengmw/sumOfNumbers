f914019672891a48cda5aa0d9bbe3c0644b90793
Matome Nkgoeng (MEA) <matome.nkgoeng@dimensiondata.com>
2019-05-08 22:30:24 +0200

    Add project files.

---
+++ API MethodssumOfNumbers/sumOfNumbers.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="sumOfNumbers.asmx.cs" Class="API_MethodssumOfNumbers.sumOfNumbers" %>
---
+++ API MethodssumOfNumbers/sumOfNumbers.asmx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace API_MethodssumOfNumbers
+{
+    /// <summary>
+    /// Summary description for sumOfNumbers
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class sumOfNumbers : System.Web.Services.WebService
+    {
+
+       
+
+        [WebMethod]
+        public int sumOfNumbersForEach(List<int> numbers)
+        {
+           
+            int Total = 0;
+
+            if (numbers.Count> 0)
+            {
+                for ( int i = 0; i < numbers.Count; i++)
+                {
+                    Total = Total + numbers[i];
+                }
+                return Total;
+            }
+            else  return 0;
+
+        }
+        [WebMethod]
+        public int sumOfNumberswhileloop(List<int> numbers)
+        {
+            int Total = 0;
+            int i = 0;
+
+            if (numbers.Count > 0)
+            {
+                while(i<numbers.Count )
+                {
+                    Total = Total + numbers[i];
+                    i++;
+
+                }
+                return Total;
+            }
+            else  return 0;
+
+        }
+
+        [WebMethod]
+        public int sumOfNumbersrecursion(List<int> numbers, int listNumbers)
+        {
+          
+            int i = numbers.Count;
+            if (listNumbers <= 0)
+            {
+                return 0;
+            }  else {
+                    return (sumOfNumbersrecursion(numbers, listNumbers-1) + numbers[listNumbers -1]);
+                   
+                }
+              
+           
+
+        }
+    }
+}
